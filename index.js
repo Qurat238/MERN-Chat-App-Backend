e0e96b9632a7cc6c7c977045d8bacc1bcce6a812
Qurat238 <q5500407@gmail.com>
2023-12-27 01:08:52 +0500

    done

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = new Server(server, {
   pingTimeout:60000,
   cors: {
-    origin:"*"
+    origin:"https://mern-chat-app-frontend-chi.vercel.app/"
   }
 })
 

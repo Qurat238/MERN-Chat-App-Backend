ba3b786936945672bce16a9427486da7845e2063
Qurat238 <q5500407@gmail.com>
2023-12-26 22:30:21 +0500

    done

--- index.js
+++ index.js
@@ -16,7 +16,12 @@ import ChatRoute from "./routes/ChatRoute.js";
 import MessageRoute from "./routes/MessageRoute.js";
 
 const app = express();
-app.use(cors());
+app.use(cors(
+  app.use(cors({
+    origin: ['https://mern-chat-app-backend-six.vercel.app/'],
+    credentials: true
+  }));
+));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));

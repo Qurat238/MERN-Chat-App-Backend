bfebb762beb5773e6caa57f7d99827f956c83568
Qurat238 <q5500407@gmail.com>
2023-12-27 00:58:39 +0500

    done

--- index.js
+++ index.js
@@ -29,9 +29,11 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(fileUpload());
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // '*' allows any origin, you can restrict it to specific origins
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Credentials", "true");
+	res.setHeader("Access-Control-Max-Age", "1800");
+	res.setHeader("Access-Control-Allow-Headers", "content-type");
+	res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
   next();
 });
 
@@ -50,7 +52,7 @@ const server = app.listen(process.env.PORT, () => {
 const io = new Server(server, {
   pingTimeout:60000,
   cors: {
-    origin:"https://mern-chat-app-frontend-chi.vercel.app/",methods:["GET","POST"],credentials:true,allowedHeaders:["Access-Control-Allow-Origin:https://mern-chat-app-frontend-chi.vercel.app/"]
+    origin:"https://mern-chat-app-frontend-chi.vercel.app/"
   }
 })
 

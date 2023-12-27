257bb145d96f7d692c7f6b733cf3e39567884c9c
Qurat238 <q5500407@gmail.com>
2023-12-27 21:43:24 +0500

    done

--- index.js
+++ index.js
@@ -17,25 +17,25 @@ import MessageRoute from "./routes/MessageRoute.js";
 
 const app = express();
 app.use(cors(
-  // {
-  //   origin:["https://mern-chat-app-frontend-chi.vercel.app/"],
-  //   methods:["POST", "GET"],
-  //   credentials:true
-  // }
+  {
+    origin:["https://mern-chat-app-frontend-chi.vercel.app/","mern-chat-app-frontend-git-main-qurat238.vercel.app","mern-chat-app-frontend-op6kq3b1k-qurat238.vercel.app"],
+    methods:["POST", "GET"],
+    credentials:true
+  }
 ));
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(fileUpload());
 
-app.use(function(req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Credentials", "true");
-	res.setHeader("Access-Control-Max-Age", "1800");
-	res.setHeader("Access-Control-Allow-Headers", "content-type");
-	res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+// 	res.setHeader("Access-Control-Allow-Credentials", "true");
+// 	res.setHeader("Access-Control-Max-Age", "1800");
+// 	res.setHeader("Access-Control-Allow-Headers", "content-type");
+// 	res.setHeader("Access-Control-Allow-Methods","PUT, POST, GET, DELETE, PATCH, OPTIONS");
+//   next();
+// });
 
 if(process.env.NODE_ENV !== "PRODUCTION"){
 // config

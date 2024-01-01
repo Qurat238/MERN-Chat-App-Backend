f0fd87aeeb828b3a763f8456daa52436fa59e666
Qurat238 <q5500407@gmail.com>
2024-01-01 19:00:33 +0500

    done

--- utils/jwttoken.js
+++ utils/jwttoken.js
@@ -9,7 +9,6 @@ const sendToken = (user, statusCode, res) => {
             Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
         ),
     };
-    console.log(token);
 
     res.status(statusCode).cookie("token", token, options).json({
         success:true,
